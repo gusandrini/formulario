d4151438999a03647b714195d619885a652c078c
gusandrini <gustavosandrini92@gmail.com>
2025-04-27 15:28:54 -0300

    ajuste

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,21 +1,31 @@
-import { TextInput, View } from "react-native";
+import { TextInput, TextInputProps, View } from "react-native";
 import {Feather} from "@expo/vector-icons";
+import { Controller, UseControllerProps } from "react-hook-form";
+
 import { styles } from "./styles";
 
 
 type InputProps = {
     icon: keyof typeof Feather.glyphMap;
+    formProps: UseControllerProps;
+    inputProps: TextInputProps;
 }
 
-export function Input({icon}: InputProps) {
+export function Input({icon, formProps, inputProps}: InputProps) {
   return (
-    <View style={styles.group}>
+    <Controller
+      render={() =>(
+
+        <View style={styles.group}>
         <View style={styles.icon}>
             <Feather name={icon} size= {24} color="red"/>
         </View>
     
-        <TextInput style={styles.control}/>
+        <TextInput style={styles.control} {...inputProps}/>
     </View>
-    
-  )
+
+      )}
+      {...formProps}
+    />
+  );
 };
