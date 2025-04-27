ceeb7c3cafe30b5a712bebb8e937b4e770991792
gusandrini <gustavosandrini92@gmail.com>
2025-04-27 14:08:51 -0300

    iniciando

---
+++ src/screens/FromStepOne/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        backgroundColor: "white",
+        justifyContent: "center",
+        padding: 24
+
+    },
+    title:{
+        fontSize: 24,
+        fontWeight: "700"
+    }
+});
